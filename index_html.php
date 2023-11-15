7943a6d6d6c15d6923946761dcd3235ffd80c11d
JuanEs <jchavespa@uninpahu.edu.co>
2023-11-15 01:14:17 -0500

    Se agrega visualizacion contactenos pagina index, falta afinar comentarios profesor y se da fin a fase 1

--- index_html.php
+++ index_html.php
@@ -65,7 +65,14 @@
         </div>
     </section>
     <section id="nosotros">
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit hic ea dicta neque! Excepturi ex laborum fugiat perferendis, dolorum atque placeat? Dolor enim officia, quibusdam sapiente aliquid deserunt ut voluptates.</p>
+        <p>El proyecto PIA, bajo el nombre de Uniproyectos, nace de la urgente necesidad de contar con un sistema especializado capaz de conservar, destacar y promover los proyectos realizados por los estudiantes. En el contexto educativo, es imperativo trascender la noción de que los proyectos desarrollados sean meros entregables destinados al olvido. Uniproyectos no solo aborda esta carencia, sino que también se presenta como una solución única diseñada para asegurar que los proyectos educativos no solo perduren con el tiempo., sino que también impulsen continuamente el aprendizaje y desarrollo académico.</p>
+        <br>
+        <p>El objetivo principal de Uniproyectos, es promover y conservar los diferentes proyectos estudiantiles para que obtengan reconocimiento y sean presentados al público en general:</p>
+        <ul>
+            <li>Realizar un análisis detallado para identificar las necesidades y limitaciones en la gestión de proyectos de Uninpahu.</li>
+            <li>Implementar una plataforma para mejorar la gestión de proyectos, destacando su conservación y promoviendo su reconocimiento al público.</li>
+            <li>Realizar un seguimiento a cada fase del proyecto, comprobando la funcionalidad de cada módulo y realizando pruebas de desarrollo para garantizar su optimo rendimiento y su conformidad a los requerimientos; así mismo que la herramienta pueda acoplarse a diversas situaciones.</li>
+        </ul>
     </section>
 </main>
 <footer>
